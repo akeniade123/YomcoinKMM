3fad88ca3e6397358c7eca7b0388dafac4f09344
akeniade123 <adeyinkaakeni@gmail.com>
2023-06-06 16:29:35 -0700

    06061629

--- shared/src/commonMain/kotlin/com/dlvtech/yomcoin/common/views/Sect.kt
+++ shared/src/commonMain/kotlin/com/dlvtech/yomcoin/common/views/Sect.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
